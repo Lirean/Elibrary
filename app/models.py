67cc2a9235970ae108fce074ca9b7f4a45173dc0
Vitaly Bilyi <lirean@localhost.localdomain>
2016-03-12 17:47:15 +0200

    added account administration

--- app/models.py
+++ app/models.py
@@ -37,7 +37,6 @@ class User(UserMixin, db.Model):
 
     def generate_confirmation_token(self, expiration=4800):
         s = Serializer(current_app.config['SECRET_KEY'], expiration)
-
         return s.dumps({'confirm': self.id})
 
     def confirm(self, token):
@@ -51,6 +50,46 @@ class User(UserMixin, db.Model):
         self.confirmed = True
         db.session.add(self)
         return True
+
+    def generate_reset_token(self, expiration=4800):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'reset': self.id})
+
+    def reset_password(self, token, new_password):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('reset') != self.id:
+            return False
+        self.password = new_password
+        db.session.add(self)
+        return True
+
+    def generate_email_change_token(self, new_email, expiration=4800):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'change_email': self.id, 'new_email': new_email})
+
+    def change_email(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('change_email') != self.id:
+            return False
+        new_email = data.get('new_email')
+        if new_email is None:
+            return False
+        if self.query.filter_by(email=new_email).first() is not None:
+            return False
+        self.email = new_email
+        db.session.add(self)
+        return True
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
     def __repr__(self):
         return '<User %r>' % self.username
