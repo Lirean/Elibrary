26f4db9679ba82f57062e91a5bec4207b172e4ff
Vitaly Bilyi <lirean@localhost.localdomain>
2016-03-13 13:23:57 +0200

    added self link for books

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,22 @@
-from flask import render_template, abort
+from flask import render_template, abort, request, current_app
 from . import main
 from ..models import User, Book
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    books = Book.query.all()
-    return render_template('index.html', books=books)
+    page = request.args.get('page', 1, type=int)
+    pagination = Book.query.order_by(Book.id).paginate(
+    page, per_page=current_app.config['FLASKY_BOOKS_PER_PAGE'],
+    error_out=False)
+    books = pagination.items
+    return render_template('index.html', books=books,
+    	                   pagination=pagination)
 
+@main.route('/book/<int:id>')
+def book(id):
+	book = Book.query.get_or_404(id)
+	return render_template('book.html', books=[book])
 
 @main.route('/user/<username>')
 def user(username):
