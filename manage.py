3353f38ac569aebf081a22950866312a19a4a40c
Vitaly Bilyi <lirean@localhost.localdomain>
2016-03-14 08:48:19 +0200

    added profile

--- manage.py
+++ manage.py
@@ -29,6 +29,14 @@ def test():
 
 
 @manager.command
+def profile(length=25, profile_dir=None):
+    """Start the application under the code profiler."""
+    from werkzeug.contrib.profiler import ProfilerMiddleware
+    app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],
+                                      profile_dir=profile_dir)
+    app.run()
+
+@manager.command
 def deploy():
     """Run deployment tasks."""
     from flask.ext.migrate import upgrade
