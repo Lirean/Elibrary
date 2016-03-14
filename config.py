986c65e6fdd16fce55e0f3fab54cd3db4f72c631
Vitaly Bilyi <lirean@localhost.localdomain>
2016-03-14 06:06:24 +0200

    add deployment func

--- config.py
+++ config.py
@@ -8,12 +8,13 @@ class Config:
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'vb2109@gmail.com'
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or '741963789123q'
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Elibrary]'
-    FLASKY_MAIL_SENDER = 'Elibrary <some@example.com>'
-    FLASKY_ADMIN = os.environ.get('ELIBRARY_ADMIN')
-    FLASKY_BOOKS_PER_PAGE = 20
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ELIBRARY_MAIL_SUBJECT_PREFIX = '[Elibrary]'
+    ELIBRARY_MAIL_SENDER = 'Elibrary <some@example.com>'
+    ELIBRARY_ADMIN = os.environ.get('ELIBRARY_ADMIN')
+    ELIBRARY_BOOKS_PER_PAGE = 20
+    MAX_SEARCH_RESULTS = 50
 
     @staticmethod
     def init_app(app):
@@ -23,15 +24,17 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
-
+    WHOOSH_BASE = SQLALCHEMY_DATABASE_URI
 
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+    WHOOSH_BASE = SQLALCHEMY_DATABASE_URI
 
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    WHOOSH_BASE = SQLALCHEMY_DATABASE_URI
 
 
 config = {
